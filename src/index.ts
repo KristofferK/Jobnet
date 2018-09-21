259747abf1ca7eb13bfdb11b29c92497bf0a6e90
Kristoffer <fobber1995@gmail.com>
2018-09-21 13:08:26 +0200

    Add print statement to show who you are logged in as

--- src/index.ts
+++ src/index.ts
@@ -18,22 +18,36 @@ class JobnetClient {
     return Promise.resolve(new JobnetClient(browser, page));
   }
 
-  async login(username: string, password: string) {
+  async login(username: string, password: string): Promise<string> {
     await this.page.goto('https://job.jobnet.dk/CV/Frontpage');
     await this.page.focus('#JobnetUsername');
     await this.page.keyboard.type(username);
     await this.page.focus('#JobnetPassword');
     await this.page.keyboard.type(password);
     await this.page.click('#LoginButton')  
-    await this.screenshot('keyboard.png');
+    await this.screenshot('01_login_button_clicked.png');
+    await this.page.waitForSelector('#TjobButton');
+    await this.screenshot('02_login_successful.png');
+    return this.getFullname();
   }
 
   private async screenshot(file: string) {
     return this.page.screenshot({ path: `${this.screenshotsFolder}/${file}` });
   }
+
+  private async getFullname() {
+    return this.page.evaluate(() => {
+      const nameTag = <HTMLSpanElement>document.querySelector('.page-area.header-area span');
+      if (nameTag !== null) {
+        return nameTag.innerText;
+      }
+      return null;
+    });
+  }
 }
 
 (async() => {
   const client = await JobnetClient.initialize();
-  await client.login(credentails.username, credentails.password);
+  const username = await client.login(credentails.username, credentails.password);
+  console.log('Logged in as', username);
 })();
